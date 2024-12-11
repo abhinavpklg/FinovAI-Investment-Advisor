b0b5bdf577815904aa435792b04e005c1bd2d7f6
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-12-10 20:21:30 -0500

    AI stock analysis

--- app.py
+++ app.py
@@ -13,15 +13,40 @@ import requests
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
+from openai import OpenAI
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone
+import utils as ut
+from langchain_pinecone import PineconeVectorStore
+from openai import OpenAI
+import json
+import concurrent.futures
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.schema import Document
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 # Load environment variables
 load_dotenv()
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 
-# Initialize Pinecone
+# Initialize Pinecone 
 index_name = "finov1"
 pinecone_index = initialize_pinecone(PINECONE_API_KEY, "us-east-1", index_name)
+
+#initialize Pinecone
+pc = Pinecone(PINECONE_API_KEY)
+index_name = "stocks"
+namespace = "stock-description_detailed"
+# Connect to the Pinecone index
+pinecone_index = pc.Index(index_name)
+
+# initialize OpenAI Client
+client = OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key=os.getenv("GROQ_API_KEY")
+)
 
 # Initialize AI model globally
 @st.cache_resource
@@ -152,6 +177,136 @@ def create_gauge_chart(value, title, min_value=0, max_value=100):
     ))
     return fig
 
+
+def format_matches(top_matches):
+    ticker_details = []
+    for match in top_matches['matches']:
+        ticker_details.append({
+            'ticker': match['metadata']['Ticker'],
+            'name': match['metadata']['Name'],
+            'business_summary':match['metadata']['Business Summary'],
+            'website':match['metadata']['Website'],
+            'revenue_growth':match['metadata']['Revenue Growth'],
+            'gross_margins':match['metadata']['Gross Margins'],
+            'target_m_price':match['metadata']['Target Mean Price'],
+            'current_price':match['metadata']['Current Price'],
+            '52weekchange': match['metadata']['52 Week Change'],
+            'sector': match['metadata']['Sector'],
+            'market_cap': match['metadata']['Market Cap'],
+            'volume': match['metadata']['Volume'],
+            'recommendation_key': match['metadata']['Recommendation Key'],
+            'text':match['metadata']['text']
+        })
+    return ticker_details
+
+def get_huggingface_embeddings(text, model_name="sentence-transformers/all-mpnet-base-v2"):
+    model = SentenceTransformer(model_name)
+    return model.encode(text)
+
+def augment_query_context(query,  top_matches_formatted):
+    context = '<CONTEXT>\n'
+    for ticker in top_matches_formatted:
+        context += f"\n\n--------\n\n {ticker['text']}\n Sector is {ticker['sector']}. \n Market Cap is {ticker['market_cap']}. \n Volume is {ticker['volume']}."
+    print(context)
+    augmented_query = f"{context} \nMY QUESTION:\n {query}"
+    return augmented_query
+
+# Perform rag
+def perform_rag(query, user_filters):
+    # embed the query
+    raw_query_embedding = get_huggingface_embeddings(query)
+
+    # apply filter to the metadata
+    if user_filters:
+        
+         market_cap= user_filters['Market Cap']
+         volume = user_filters['Volume']
+         recommendation_keys = user_filters['Recommendation Keys']
+        
+         filter= {"$and": [
+            {"Market Cap": {"$gte": market_cap}},
+            {"Volume": {"$gte": volume}},
+            {"52 Week Change": {"$gte": -0.2}},
+            {"Recommendation Key": {"$in": recommendation_keys }}
+         ]}
+    else:      
+        filter= {"$and": [
+            {"52 Week Change": {"$gt": 0}}, 
+            {"Recommendation Key": {"$in": ["buy", "strong buy", "hold"] }},
+            ]
+        }
+
+    # print("filter: ", filter)    
+        
+    # find the top matches
+    top_matches = pinecone_index.query(vector=raw_query_embedding.tolist(), filter = filter, top_k = 12, include_metadata=True, namespace=namespace)
+    top_matches_formatted = format_matches(top_matches)
+
+    print(top_matches)
+
+    augmented_query = augment_query_context(query, top_matches_formatted)
+
+
+    system_prompt = """You are a financial expert at providing answers about stocks. Please answer my question provided.
+            Analyze the stocks' in detail and explain current performance and potential future trends.
+            Identify any notable connections or relationships with other stocks (e.g., industry, market correlation, or shared factors).
+            Provide a concise, actionable insight to guide investment decisions.
+    """
+    try:
+        llm_response = client.chat.completions.create(
+            model = 'llama-3.1-70b-versatile',
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": augmented_query}
+            ]
+        )
+    except:
+        llm_response = client.chat.completions.create(
+                model = 'llama-3.1-8b-instant',
+                messages=[
+                    {"role": "system", "content": system_prompt},
+                    {"role": "user", "content": augmented_query}
+                ]
+            )
+
+
+    response = llm_response.choices[0].message.content
+    return top_matches_formatted, response
+
+def render_stock_block(stock):
+    st.markdown("""<style>.small-font {font-size:12px; color:rgb(128, 128, 128);} a::after { content: none; }</style>""", unsafe_allow_html=True)
+    st.markdown("""<style>.number-font {font-size:20px; font-weight:bold;} a::after { content: none;}</style>""", unsafe_allow_html=True)
+    
+    # Create a container for the stock block
+    with st.container():
+        st.markdown(f"#### {stock['name']} ({stock['ticker']})")
+        st.markdown(f"Website: {stock['website']}", unsafe_allow_html=True)
+
+        # Create columns for displaying stock information
+        cols = st.columns(3)
+        with cols[0]:
+            st.markdown("""<div class='small-font'>Revenue Growth</div>""", unsafe_allow_html=True)
+            st.markdown(f"""<div class='number-font'>{ut.safe_format(stock['revenue_growth'])}</div>""", unsafe_allow_html=True)
+            st.markdown("""<div class='small-font'>Gross Margins</div>""", unsafe_allow_html=True)
+            st.markdown(f"""<div class='number-font'>{ut.safe_format(stock['gross_margins'])}</div>""", unsafe_allow_html=True)
+
+        with cols[1]:
+            st.markdown("""<div class='small-font'>Market Cap</div>""", unsafe_allow_html=True)
+            market_cap = ut.format_large_number(stock['market_cap'])
+            st.markdown(f"""<div class='number-font'>{market_cap}</div>""", unsafe_allow_html=True)
+            st.markdown("""<div class='small-font'>Volume</div>""", unsafe_allow_html=True)
+            volume = ut.format_large_number(stock['volume'])
+            st.markdown(f"""<div class='number-font'>{volume}</div>""", unsafe_allow_html=True)
+
+        with cols[2]:
+            st.markdown("""<div class='small-font'>Valuation</div>""", unsafe_allow_html=True)
+            valuation = (stock['target_m_price'] - stock['current_price']) / stock['current_price']
+            valuation = ut.format_colored_number(valuation)
+            st.markdown(f"""<div class='number-font'>{valuation}</div>""", unsafe_allow_html=True)
+            st.markdown("""<div class='small-font'>52 Week Change</div>""", unsafe_allow_html=True)
+            weekchange = ut.format_colored_number(stock['52weekchange'])
+            st.markdown(f"""<div class='number-font'>{weekchange}</div>""", unsafe_allow_html=True)
+
 def main():
     st.set_page_config(layout="wide")
     create_header()
@@ -159,7 +314,7 @@ def main():
     main_col, timeline_col = st.columns([0.75, 0.25])
 
     with main_col:
-        tab1, tab2 = st.tabs(["📊 Ticker Information", "💬 Chat with FinovAI"])
+        tab1, tab2, tab3 = st.tabs(["📊 Ticker Information", "💬 Chat with FinovAI", "📉 AI Stock Analysis"])
 
         with tab1:
             st.title("Financial Market Dashboard")
@@ -298,6 +453,67 @@ def main():
 
             if st.button("Reset Chat"):
                 st.session_state["history"] = []
+
+        with tab3:
+            st.title("📉 AI Stock Analysis")
+            st.caption("Get Automated Stock Analysis done right here!")
+            user_query = st.text_area(
+                "Enter a description for the kind of stocks you are looking for:",
+                placeholder="Type here"
+            )
+
+            with st.expander("Apply filters"):
+                # Market Cap Range
+                market_cap = st.number_input(
+                    "Market Cap",
+                    min_value=0,
+                    max_value=10000000000000,
+                    value=1000000,
+                    step=1000
+                )
+
+                # Volume Range
+                volume = st.number_input(
+                    "Volume",
+                    min_value=0,
+                    max_value=1000000000,
+                    value=10000,
+                    step=100
+                )
+
+                # Recommendation Key
+                recommendation_keys = ["strong buy", "buy", "hold", "sell", "strong sell"]
+                selected_recommendation_keys = st.multiselect(
+                    "Recommendation Keys:",
+                    recommendation_keys,
+                    ["strong buy", "buy", "hold"]
+                )
+
+                user_filters = {
+                    "Market Cap": market_cap,
+                    "Volume": volume,
+                    "Recommendation Keys": selected_recommendation_keys
+                }
+
+            if st.button("Find Stocks"):
+                top_matches, results = perform_rag(user_query, user_filters)
+                with st.container():
+                    if top_matches:
+                        cols_main = st.columns(2)
+                        with cols_main[0]:
+                            for match in top_matches[:3]:
+                                render_stock_block(match)
+
+                        with cols_main[1]:
+                            for match in top_matches[3:6]:
+                                render_stock_block(match)
+                    else:
+                        st.write("No stocks found. Please refine your query.")
+
+                    st.divider()
+
+                    st.write("## Analysis")
+                    st.write(results)
 
     create_footer()
 
